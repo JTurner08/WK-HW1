3944d06af5bcc8031e7b923d97837c503c6e6d32
Jarquise Turner <80004108+JTurner08@users.noreply.github.com>
2021-08-26 08:56:27 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+
+import topbar from './topbar';
+import sidebar from './sidebar'
+
+import reviews from './Reviews'
+
+
+
+function App() {
+  return (
+  <div className="App">
+
+    <topbar/>
+    <sidebar/>
+    <Reviews/>
+    </div>
+
+
+  );
+}
+
+export default App;
